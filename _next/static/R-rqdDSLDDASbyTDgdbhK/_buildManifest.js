0670fa13efefadeeced50cdeeb5701776b4049d9
Wallace Mwabini <wallace@rawle.systems>
2023-05-18 06:54:19 +0300

    Initial Commit

---
+++ _next/static/R-rqdDSLDDASbyTDgdbhK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/709-d570a7ce7ba7b60a.js","static/css/1ef8a9201d56b0b3.css","static/chunks/pages/index-45e7b43b312c2295.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
